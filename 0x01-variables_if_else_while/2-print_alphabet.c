519e6d3268cb8baa6227906a5b479abcf7e70274
Ghizlaneeeé <gmoublad@gmail.com>
2023-05-26 03:28:16 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - the last digit
+ * main - write a programme that prints the alphabet in lowercase
  * Return: 0
  */
 
 int main(void)
 {
-	int n, x;
+	int a = 97;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-
-	if (x > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, x);
-
-	else if (x < 6 && x != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
-
-	else
-		printf("Last digit of %d is %d and is 0\n", n, x);
-
-
+	while (a < 123)
+	{
+		putchar((char) a);
+		a++;
+	}
+	putchar('\n');
 	return (0);
 }
